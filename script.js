fea9120a2bd9aea5f371b273db23cfaa379283af
Artak Sargsyan <artak.asv@gmail.com>
2022-10-28 00:05:49 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 256;
+    if (offset > 1256) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 256;
+    if (offset < 0) {
+        offset = 1256
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+
+
